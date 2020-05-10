4254a6de880b5b914d681e8220dbf222a0bd66d4
itheimaxia <xiadong@itcast.cn>
2020-05-10 19:59:26 +0800

    添加注释

--- test-spring-boot-autoconfig/src/main/java/com/brianxia/testspringbootautoconfig/TestAutoConfiguration.java
+++ test-spring-boot-autoconfig/src/main/java/com/brianxia/testspringbootautoconfig/TestAutoConfiguration.java
@@ -10,12 +10,18 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//声明这个类是一个配置类，在类中的方法可以使用@Bean返回一个对象，将对象自动加载到ioc容器中
 @Configuration
+//判断类加载路径中是否包含API的依赖，ItheimaApi类
 @ConditionalOnClass(ItheimaApi.class)
 public class TestAutoConfiguration {
+
+    //@Bean可以将方法返回的对象注入到Spring容器中，但是受限于conditional注解
     @Bean
+    //判断接口的实现类是否在容器中，如果存在，不创建。如果不存在，则创建
     @ConditionalOnMissingBean(ItheimaApi.class)
-    //matchIfMissing指没有配置时的默认值
+    //ConditionalOnProperty作用是根据配置来判断 name属性代表配置项 havingValue指定值
+    //if(itcast.framework == "a") return true; else return false matchIfMissing指没有配置时的默认值  true
     @ConditionalOnProperty(name = "itcast.framework", havingValue = "a", matchIfMissing = true)
     public ItheimaApi getATemplate() {
         System.out.println("自动装配加载A框架啦！");
